c4805b6883c09977a43f584d21380864e5565587
yuanquanlu <yuanquanlu@gmail.com>
2017-03-15 10:53:06 +0800

    complete part III

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,10 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"log"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -32,5 +36,48 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+
+
+	var wg sync.WaitGroup
+	for id := 0; id < ntasks; id++ {
+
+		log.Println("----------", id)
+		wg.Add(1) // 每次开启新的goroutine时，增加1
+		go func(id int) {
+			defer wg.Done() // goroutine结束时，减一
+
+			// 对于同一task 会反复去分配worker 处理 worker fail
+			//for {
+				addr := <- registerChan // 取出worker addr 要重新放回
+				log.Println("worker addr: ", addr, "id: ", id, "phase", phase)
+				fileName := ""
+				if phase == mapPhase {
+					fileName = mapFiles[id]
+				}
+
+				tmp := DoTaskArgs{JobName:jobName, File:fileName, Phase:phase, TaskNumber: id, NumOtherPhase:n_other}
+				call(addr, "Worker.DoTask", tmp, nil)
+
+				//if !ok {
+				//	// worker没有响应，继续分配
+				//	log.Println("DoTask error", addr)
+				//} else {
+				//	// worker响应，break，处理下一个task
+
+
+					// 需要goroutine，否则死锁，part III 就需要
+					go func() { // 重新将addr加入Chan
+						registerChan <- addr
+					}()
+
+
+					//break
+//				}
+			//}
+
+		}(id)
+	}
+
+	wg.Wait() // 等待所有goroutine 结束
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
