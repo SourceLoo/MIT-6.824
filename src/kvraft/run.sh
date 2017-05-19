d1ef9b5b08829bad353d120e80dffa8dd1910ca2
yuanquanlu <yuanquanlu@gmail.com>
2017-05-20 02:02:01 +0800

    host_db is the same with mac

--- src/kvraft/run.sh
+++ src/kvraft/run.sh
@@ -6,6 +6,8 @@
 #name=TestSnapshotUnreliable
 name=Test
 
+export GOPATH=/home/users/luyq/workspace/6.824
+
 go test -timeout 40m -run $name > log00 2>&1 &
 go test -timeout 40m -run $name > log01 2>&1 &
 go test -timeout 40m -run $name > log02 2>&1 &
@@ -26,3 +28,66 @@ go test -timeout 40m -run $name > log16 2>&1 &
 go test -timeout 40m -run $name > log17 2>&1 &
 go test -timeout 40m -run $name > log18 2>&1 &
 go test -timeout 40m -run $name > log19 2>&1 &
+
+go test -timeout 40m -run $name > log20 2>&1 &
+go test -timeout 40m -run $name > log21 2>&1 &
+go test -timeout 40m -run $name > log22 2>&1 &
+go test -timeout 40m -run $name > log23 2>&1 &
+go test -timeout 40m -run $name > log24 2>&1 &
+go test -timeout 40m -run $name > log25 2>&1 &
+go test -timeout 40m -run $name > log26 2>&1 &
+go test -timeout 40m -run $name > log27 2>&1 &
+go test -timeout 40m -run $name > log28 2>&1 &
+go test -timeout 40m -run $name > log29 2>&1 &
+go test -timeout 40m -run $name > log30 2>&1 &
+go test -timeout 40m -run $name > log31 2>&1 &
+go test -timeout 40m -run $name > log32 2>&1 &
+go test -timeout 40m -run $name > log33 2>&1 &
+go test -timeout 40m -run $name > log34 2>&1 &
+go test -timeout 40m -run $name > log35 2>&1 &
+go test -timeout 40m -run $name > log36 2>&1 &
+go test -timeout 40m -run $name > log37 2>&1 &
+go test -timeout 40m -run $name > log38 2>&1 &
+go test -timeout 40m -run $name > log39 2>&1 &
+
+go test -timeout 40m -run $name > log40 2>&1 &
+go test -timeout 40m -run $name > log41 2>&1 &
+go test -timeout 40m -run $name > log42 2>&1 &
+go test -timeout 40m -run $name > log43 2>&1 &
+go test -timeout 40m -run $name > log44 2>&1 &
+go test -timeout 40m -run $name > log45 2>&1 &
+go test -timeout 40m -run $name > log46 2>&1 &
+go test -timeout 40m -run $name > log47 2>&1 &
+go test -timeout 40m -run $name > log48 2>&1 &
+go test -timeout 40m -run $name > log49 2>&1 &
+go test -timeout 40m -run $name > log50 2>&1 &
+go test -timeout 40m -run $name > log51 2>&1 &
+go test -timeout 40m -run $name > log52 2>&1 &
+go test -timeout 40m -run $name > log53 2>&1 &
+go test -timeout 40m -run $name > log54 2>&1 &
+go test -timeout 40m -run $name > log55 2>&1 &
+go test -timeout 40m -run $name > log56 2>&1 &
+go test -timeout 40m -run $name > log57 2>&1 &
+go test -timeout 40m -run $name > log58 2>&1 &
+go test -timeout 40m -run $name > log59 2>&1 &
+
+go test -timeout 40m -run $name > log60 2>&1 &
+go test -timeout 40m -run $name > log61 2>&1 &
+go test -timeout 40m -run $name > log62 2>&1 &
+go test -timeout 40m -run $name > log63 2>&1 &
+go test -timeout 40m -run $name > log64 2>&1 &
+go test -timeout 40m -run $name > log65 2>&1 &
+go test -timeout 40m -run $name > log66 2>&1 &
+go test -timeout 40m -run $name > log67 2>&1 &
+go test -timeout 40m -run $name > log68 2>&1 &
+go test -timeout 40m -run $name > log69 2>&1 &
+go test -timeout 40m -run $name > log70 2>&1 &
+go test -timeout 40m -run $name > log71 2>&1 &
+go test -timeout 40m -run $name > log72 2>&1 &
+go test -timeout 40m -run $name > log73 2>&1 &
+go test -timeout 40m -run $name > log74 2>&1 &
+go test -timeout 40m -run $name > log75 2>&1 &
+go test -timeout 40m -run $name > log76 2>&1 &
+go test -timeout 40m -run $name > log77 2>&1 &
+go test -timeout 40m -run $name > log78 2>&1 &
+go test -timeout 40m -run $name > log79 2>&1 &
